5596ed0ad5dece747705d7c22ea53920f829672e
Harsh <harsh0030sahu@gmail.com>
2024-04-06 20:18:36 +0530

    more features design

--- src/components/SliderMenu.jsx
+++ src/components/SliderMenu.jsx
@@ -2,11 +2,15 @@ import { IoMdClose } from "react-icons/io";
 import Logo from "../assets/logo.png";
 import Location from "./Location";
 import { categories } from "../data/category";
-import { IoChevronDownSharp } from "react-icons/io5";
+import { IoChevronDownSharp, IoChevronUpSharp } from "react-icons/io5";
 import propTypes from "prop-types";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import Categories from "./Categories";
 
 const SliderMenu = ({ setToggleSideMenu, toggleSideMenu }) => {
+  //state
+  const [toggleGender, setToggleGender] = useState("");
+
   //useRef
   const slideMenuVisible = useRef();
 
@@ -43,9 +47,35 @@ const SliderMenu = ({ setToggleSideMenu, toggleSideMenu }) => {
           {categories &&
             categories.map((item, i) => (
               <li key={i}>
-                <span>
-                  {item.gender} <IoChevronDownSharp />
-                </span>
+                {item.gender === toggleGender ? (
+                  <span onClick={() => setToggleGender("")}>
+                    {item.gender}
+                    <IoChevronUpSharp />
+                  </span>
+                ) : (
+                  <span onClick={() => setToggleGender(item.gender)}>
+                    {item.gender}
+                    <IoChevronDownSharp />
+                  </span>
+                )}
+
+                {toggleGender === item.gender ? (
+                  <div className="bottom fade">
+                    {Categories && (
+                      <>
+                        <ul>
+                          {categories
+                            .find((g) => g.gender === "men")
+                            .types.map((item, i) => (
+                              <li key={i}>{item}</li>
+                            ))}
+                        </ul>
+                      </>
+                    )}
+                  </div>
+                ) : (
+                  ""
+                )}
               </li>
             ))}
 
